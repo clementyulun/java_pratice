fd6d7a06a648f700dfa37ef3ba0bab8b689ea0e3
picarico <picarico@localhost>
2021-10-31 15:51:45 +0800

    implement bubble sort

--- Hello World/src/pratice/java/BubbleSort.java
+++ Hello World/src/pratice/java/BubbleSort.java
@@ -1,10 +1,22 @@
 package pratice.java;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		//  TODO Auto-generated method stub
-
+		int[] arr = {4,5,10,23,5,2,23,5,2,1,5,3,7,6,63234,74,85};
+		int temp = 0;
+		for(int i = 0; i < arr.length - 2;  i++) {
+			for(int j = 1; j < arr.length - i; j++) {
+				if(arr[j] < arr[j-1]) {
+					temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}
+			}
+		}
+		System.out.print(Arrays.toString(arr));
 	}
 
 }
